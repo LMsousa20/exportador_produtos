65c1c664f1989cec001487f5ece0367d95e30f7d
Lucas <lmsousa20@gmail.com>
2023-12-13 18:00:38 -0300

    1312

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 require('dotenv').config()
 const { Pool } = require('pg');
 const PORTA = 3001;
-let NovoCodigoInterno = 5555;
+let NovoCodigoInterno = 0;
 const app = express()
 app.use(express.json())
 app.use(cors())
@@ -17,138 +17,10 @@ let produtosDivergentes = [];
 let produtosEmpresaDivergentes = [];
 let empareamentoDosGrupos = [];
 
-////////////////////////
-let x =[
-    {
-      subgruposExportadosOriginal: '0101',
-      subgruposExportadosSubstituido: '0101'
-    },
-    {
-      subgruposExportadosOriginal: '0102',
-      subgruposExportadosSubstituido: '0102'
-    },
-    {
-      subgruposExportadosOriginal: '0201',
-      subgruposExportadosSubstituido: '0201'
-    },
-    {
-      subgruposExportadosOriginal: '0202',
-      subgruposExportadosSubstituido: '0202'
-    },
-    {
-      subgruposExportadosOriginal: '0203',
-      subgruposExportadosSubstituido: '0203'
-    },
-    {
-      subgruposExportadosOriginal: '0204',
-      subgruposExportadosSubstituido: '0204'
-    },
-    {
-      subgruposExportadosOriginal: '0205',
-      subgruposExportadosSubstituido: '0205'
-    },
-    {
-      subgruposExportadosOriginal: '0206',
-      subgruposExportadosSubstituido: '0206'
-    },
-    {
-      subgruposExportadosOriginal: '0207',
-      subgruposExportadosSubstituido: '0207'
-    },
-    {
-      subgruposExportadosOriginal: '0208',
-      subgruposExportadosSubstituido: '0208'
-    },
-    {
-      subgruposExportadosOriginal: '0209',
-      subgruposExportadosSubstituido: '0101'
-    },
-    {
-      subgruposExportadosOriginal: '0301',
-      subgruposExportadosSubstituido: '0301'
-    },
-    {
-      subgruposExportadosOriginal: '0302',
-      subgruposExportadosSubstituido: '0302'
-    },
-    {
-      subgruposExportadosOriginal: '0303',
-      subgruposExportadosSubstituido: '0303'
-    },
-    {
-      subgruposExportadosOriginal: '0304',
-      subgruposExportadosSubstituido: '0304'
-    },
-    {
-      subgruposExportadosOriginal: '0401',
-      subgruposExportadosSubstituido: '0401'
-    },
-    {
-      subgruposExportadosOriginal: '0402',
-      subgruposExportadosSubstituido: '0402'
-    },
-    {
-      subgruposExportadosOriginal: '0403',
-      subgruposExportadosSubstituido: '0403'
-    },
-    {
-      subgruposExportadosOriginal: '0404',
-      subgruposExportadosSubstituido: '0404'
-    },
-    {
-      subgruposExportadosOriginal: '1001',
-      subgruposExportadosSubstituido: '1001'
-    },
-    {
-      subgruposExportadosOriginal: '1002',
-      subgruposExportadosSubstituido: '1002'
-    },
-    {
-      subgruposExportadosOriginal: '1003',
-      subgruposExportadosSubstituido: '1003'
-    },
-    {
-      subgruposExportadosOriginal: '1004',
-      subgruposExportadosSubstituido: '1004'
-    },
-    {
-      subgruposExportadosOriginal: '1005',
-      subgruposExportadosSubstituido: '1005'
-    },
-    {
-      subgruposExportadosOriginal: '1006',
-      subgruposExportadosSubstituido: '1006'
-    },
-    {
-      subgruposExportadosOriginal: '1801',
-      subgruposExportadosSubstituido: '1801'
-    },
-    {
-      subgruposExportadosOriginal: '1802',
-      subgruposExportadosSubstituido: '0101'
-    },
-    {
-      subgruposExportadosOriginal: '1901',
-      subgruposExportadosSubstituido: '0101'
-    },
-    {
-      subgruposExportadosOriginal: '2001',
-      subgruposExportadosSubstituido: '0101'
-    },
-    {
-      subgruposExportadosOriginal: '2002',
-      subgruposExportadosSubstituido: '0101'
-    }
-  ]
-
-  empareamentoDosGrupos = x;
-  console.log(empareamentoDosGrupos)
-
-  //////////////////////////
 
 app.listen(PORTA, () => {
     comunicacao = true
-    console.log(`conectado back-end`, PORTA)
+    // console.log(`conectado back-end`, PORTA)
 })
 
 function PrincipalBD(local) {
@@ -178,7 +50,7 @@ initDonwloadBD()
 async function initDonwloadBD() {
     await PrincipalConsult()
     await ExportConsult()
-    atualizando()
+    
 }
 
 async function ExportConsult() {
@@ -196,11 +68,10 @@ async function ExportConsult() {
         subgruposExportados = result4.rows
         const result5 = await client.query(`select * from departamentos order by codigo`)
         departamentosExportados = result5.rows
-        // console.table(produtosExportados)
-        console.log('passou')
+        // console.log('passou')
     } catch {
-        console.log('errou no principal')
-        return console.log(result1)
+        // console.log('errou no principal')
+        // return console.log(result1)
 
     }
 }
@@ -211,7 +82,7 @@ async function PrincipalConsult() {
         const result1 = await client.query(`SELECT * FROM produtos`)
         // console.table(result1.rows)
         produtosPrincipais = result1.rows
-        console.log('passou')
+        // console.log('passou')
         const result3 = await client.query(`SELECT * FROM grupos order by codigo  `)
         const result4 = await client.query(`SELECT * FROM subgrupos order by codigo `)
         gruposPrincipal = result3.rows
@@ -220,8 +91,8 @@ async function PrincipalConsult() {
         departamentosExportados = result5.rows
        
     } catch {
-        console.log('errou no principal')
-        return console.log(result1)
+        // console.log('errou no principal')
+        // return console.log(result1)
     }
 }
 
@@ -237,8 +108,8 @@ app.get('/grupos', async (req, res) => {
 
 app.post('/grupos', async (req, res) => {
     try {
-        console.log('post chegou')
-        console.log(req.body)
+        // console.log('post chegou')
+        // console.log(req.body)
         empareamentoDosGrupos = req.body
         let resposta = {message:'deu certo'}
         atualizando()
@@ -248,51 +119,64 @@ app.post('/grupos', async (req, res) => {
     }
 });
 
-//////////////////////////////////////////////////////
 
 async function atualizando() {
-    console.log('atualizando')
+    // console.log('atualizando')
 
     produtosExportados.forEach((idProdutosExportados) => {
         let encontrado = false;
 
         produtosPrincipais.filter((idProdutosPrincipais) => {
             if (idProdutosPrincipais.cod_barras == idProdutosExportados.cod_barras) {
-                // idProdutosExportados.Novo_codigo = idProdutosPrincipais.codigo
+
+             if(idProdutosPrincipais.descricao == idProdutosExportados.descricao){
+
+                let log = `${idProdutosPrincipais.descricao} analisar esses produto`
+                // console.log(log)
+                fs.appendFile('exportacao/Verificar.csv', String(log), function (err) {
+                  if (err) throw err;
+                  // console.log('Salvo os produtos em Comum');
+              });
+              }
+
+                idProdutosExportados.Novo_codigo = idProdutosPrincipais.codigo
                 produtosEmComum.push(idProdutosExportados)
+                // console.log('produto em comum')
                 encontrado = true;
+              }
+              
             }
-
-        }
-        )
-        if (!encontrado) {
-
-            if (NovoCodigoInterno == 0) {
+            )
+            if (!encontrado) {
+              
+              if (NovoCodigoInterno == 0) {
                 NovoCodigoInterno = produtosPrincipais.length;
-            }
-            NovoCodigoInterno++
-            idProdutosExportados.codigo = '0000' + NovoCodigoInterno
-            idProdutosExportados.cod_marca = '0001'
-            
-            let novoSubGrupo = empareamentoDosGrupos.find(x => x.subgruposExportadosOriginal == idProdutosExportados.cod_subgrupo)
-            // console.log(novoSubGrupo.subgruposExportadosSubstituido)
-            idProdutosExportados.cod_subgrupo = novoSubGrupo.subgruposExportadosSubstituido;
-            idProdutosExportados.cod_grupo = novoSubGrupo.subgruposExportadosSubstituido.slice(0,2)
-            // console.log(idProdutosExportados.descricao,idProdutosExportados.cod_grupo,idProdutosExportados.cod_subgrupo)
-
-            
-            produtosEmpresaExportados.forEach((pee) => {
+              }
+              NovoCodigoInterno++
+              idProdutosExportados.codigo = '0000000' + NovoCodigoInterno
+              idProdutosExportados.cod_marca = '0001'
+              
+              let novoSubGrupo = empareamentoDosGrupos.find(x => x.subgruposExportadosOriginal == idProdutosExportados.cod_subgrupo)
+              // console.log(novoSubGrupo.subgruposExportadosSubstituido)
+              idProdutosExportados.cod_subgrupo = novoSubGrupo.subgruposExportadosSubstituido;
+              idProdutosExportados.cod_grupo = novoSubGrupo.subgruposExportadosSubstituido.slice(0,2)
+              // console.log(idProdutosExportados.descricao,idProdutosExportados.cod_grupo,idProdutosExportados.cod_subgrupo)
+              // console.log('produto em Divergente')
+              produtosDivergentes.push(idProdutosExportados)
+              
+              
+              produtosEmpresaExportados.forEach((pee) => {
                 pee.cod_depto = '002'
                 if (pee.cod_barras == idProdutosExportados.cod_barras) {
-                    pee.cod_produto = idProdutosExportados.codigo
-
-                    produtosEmpresaDivergentes.push(pee)
-                    // console.log('pee adicionado', pee.cod_produto, 'cod de barras', pee.cod_barras)
+                  pee.cod_produto = idProdutosExportados.codigo
+                  pee.cod_fornecedor = '0001'
+                  produtosEmpresaDivergentes.push(pee)
+                  
+                  // console.log('pee adicionado', pee.cod_produto, 'cod de barras', pee.cod_barras)
                 }
-            })
-
+              })
+              
             // console.log( NovoCodigoInterno , typeof NovoCodigoInterno)
-            produtosDivergentes.push(idProdutosExportados)
         }
     }
     )
@@ -312,18 +196,18 @@ async function atualizando() {
 
     fs.appendFile('exportacao/emComum.csv', String(DadosProdutosEmComum), function (err) {
         if (err) throw err;
-        console.log('Salvo os produtos em Comum');
+        // console.log('Salvo os produtos em Comum');
     });
 
     fs.appendFile('exportacao/Divergentes.csv', String(DadosProdutosDivergentes), function (err) {
         if (err) throw err;
-        console.log('Salvo os produtos divergentes');
+        // console.log('Salvo os produtos divergentes');
     });
     // console.log(produtosEmComum)
-    console.table('So pra separa dos produtos divergentes')
-    console.table(produtosDivergentes.length);
+    //console.table('So pra separa dos produtos divergentes')
+    // console.table(produtosDivergentes.length);
 
-    // emparelhamento()
+    
 
     MakeInsert()
     ProdutosEmpresaMakeInsert(produtosEmpresaExportados)
@@ -357,7 +241,7 @@ function MakeInsert() {
 
     fs.appendFile('exportacao/Isert_divergentes.txt', String(comando), function (err) {
         if (err) throw err;
-        console.log('Salvo o Insert');
+        // console.log('Salvo o Insert');
     });
 
 
@@ -380,26 +264,26 @@ function ProdutosEmpresaMakeInsert(entrada) {
         let valoresItens = '';
         array.forEach((valorProduto, idx) => {
             if (idx !== 0) { valoresItens += ',' }
-            valoresItens += `'${produtoComando[valorProduto]}'`;
+              
+          
+            if(produtoComando[valorProduto] === null){
+              valoresItens += `NULL`;
+            }else {
+              console.log(produtoComando[valorProduto].length, typeof produtoComando[valorProduto])
+              valoresItens += `'${produtoComando[valorProduto]}'`;
+            }
         })
 
         comando += `INSERT INTO produtos_empresa(${array}) values (${valoresItens});\n`
-        //         console.log(idc, `${entrada.length}`)
+                // console.log(idc, `${entrada.length}`)
     })
 
     fs.appendFile('exportacao/Isert_divergentespee.txt', String(comando), function (err) {
         if (err) throw err;
-        console.log('Salvo o Insert dos Produtos empresa');
+        // console.log('Salvo o Insert dos Produtos empresa');
     });
 
 
 }
 
-// async function emparelhamento(){
-//     let produtosDivergentesCorrigos = [];
-//     produtosDivergentes.forEach((idProdutosExportados)=>{
-       
-//     })
 
-    
-// }
